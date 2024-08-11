8e6176d2173a075c3663f1b7285d834e8772c077
aishwarya-soni <159514684+Aishwaryaabesec@users.noreply.github.com>
2024-08-11 20:47:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ function signup() {
     var passwordInput = document.getElementById("password");
     var confirmPasswordInput = document.getElementById("confirm-password");
   
-    // Perform validation and signup logic here
-    // You can use JavaScript to validate the form inputs and create an account
     console.log("Name:", nameInput.value);
     console.log("Email:", emailInput.value);
     console.log("Password:", passwordInput.value);
     console.log("Confirm Password:", confirmPasswordInput.value);
   
-    // Reset the form after successful signup
+
     nameInput.value = "";
     emailInput.value = "";
     passwordInput.value = "";
